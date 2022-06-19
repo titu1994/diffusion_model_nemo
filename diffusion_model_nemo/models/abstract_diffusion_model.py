Fix issues with calculation of bits per dimension - missing last timestep of t, wrong coefficient for posterior_log_variance_clipped,
Signed-off-by: Somshubra Majumdar <titu1994@gmail.com>

diffusion_model_nemo/models/abstract_diffusion_model.py
@@ -146,7 +146,7 @@ class AbstractDiffusionModel(ModelPT):
             t_b = torch.full([B], fill_value=T, device=x_start.device, dtype=torch.long)
             zero_tensor = torch.tensor(0.0, device=x_start.device)
 
-            for t in tqdm(range(T - 1, 0, -1), desc='Computing bits per dimension', total=T):
+            for t in tqdm(range(T - 1, -1, -1), desc='Computing bits per dimension', total=T):
                 t_b = t_b * 0 + t
 
                 x_t = self.sampler.q_sample(x_start=x_start, t=t_b)
@@ -182,6 +182,7 @@ class AbstractDiffusionModel(ModelPT):
 
             # Compute total bpd
             total_bpd_b = torch.sum(terms_buffer, dim=1) + prior_bpd_b
+
 
         # assert terms_buffer.shape == mse_buffer.shape == [B, T] and total_bpd_b.shape == prior_bpd_b.shape == [B]
         result = {

diffusion_model_nemo/models/ddpm.py
@@ -100,8 +100,7 @@ class DDPM(AbstractDiffusionModel):
         terms_bpd = torch.sum(torch.stack([x['terms_bpd'] for x in outputs])) / total_samples
         prior_bpd = torch.sum(torch.stack([x['prior_bpd'] for x in outputs])) / total_samples
 
-        result = {'test_total_bpd (per channel)': total_bpd, 'test_terms_bpd': terms_bpd, 'test_prior_bpd': prior_bpd,
-                  'test_total_bpd': total_bpd * self.channels}
+        result = {'test_total_bpd': total_bpd, 'test_terms_bpd': terms_bpd, 'test_prior_bpd': prior_bpd}
         self.log_dict(result)
 
         return result

diffusion_model_nemo/modules/gaussian_diffusion.py
@@ -22,7 +22,7 @@ class GaussianDiffusion(AbstractDiffusionProcess):
         schedule_name: str,
         schedule_cfg: Optional[DictConfig] = None,
         objective: str = "pred_noise",
-        class_conditional: bool = False
+        class_conditional: bool = False,
     ):
         super().__init__(timesteps=timesteps, schedule_name=schedule_name, schedule_cfg=schedule_cfg)
 
@@ -73,7 +73,9 @@ class GaussianDiffusion(AbstractDiffusionProcess):
         # above: equal to 1. / (1. / (1. - alpha_cumprod_tm1) + alpha_t / beta_t)
         self.posterior_variance = self.betas * (1.0 - self.alphas_cumprod_prev) / (1.0 - self.alphas_cumprod)
         # below: log calculation clipped because the posterior variance is 0 at the beginning of the diffusion chain
-        self.posterior_log_variance_clipped = torch.log(torch.maximum(self.posterior_variance, torch.tensor(1e-20)))
+        self.posterior_log_variance_clipped = torch.log(
+            torch.cat([self.posterior_variance[1].unsqueeze(0), self.posterior_variance[1:]])
+        )
         self.posterior_mean_coef1 = self.betas * torch.sqrt(self.alphas_cumprod_prev) / (1.0 - self.alphas_cumprod)
         self.posterior_mean_coef2 = (
             (1.0 - self.alphas_cumprod_prev) * torch.sqrt(self.alphas) / (1.0 - self.alphas_cumprod)
@@ -81,7 +83,7 @@ class GaussianDiffusion(AbstractDiffusionProcess):
 
     def q_mean_variance(self, x_start: torch.Tensor, t: torch.Tensor):
         mean = x_start * self.extract(self.sqrt_alphas_cumprod, t, x_start.shape)
-        variance = self.extract(1. - self.alphas_cumprod, t, x_start.shape)
+        variance = self.extract(1.0 - self.alphas_cumprod, t, x_start.shape)
         log_variance = self.extract(self.log_one_minus_alphas_cumprod, t, x_start.shape)
         return mean, variance, log_variance
 